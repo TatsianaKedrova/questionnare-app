844a93f402b00406a4f7e42f6795f8a4b17a03f8
TatsianaKedrova <Baletrot@gmail.com>
2022-03-17 16:04:39 +0200

    feat(vers-0.1): installed jest library and wrote initial test construction

---
+++ src/bll/questionsReducer.spec.ts
@@ -0,0 +1,11 @@
+export {};
+
+
+//didn't finish it yet
+describe("CircularBuffer", () => {
+  test("setting the radio button value correctly", () => {});
+
+  test("setting the birthdate correctly", () => {});
+
+
+});
